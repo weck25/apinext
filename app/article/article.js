016c919d9ddfec0fdee00910266f12b0efcf57a8
unknown <weck2505@gmail.com>
2023-11-27 18:42:17 +0700

    add SWR

--- app/article/article.js
+++ app/article/article.js
@@ -1,95 +1,58 @@
-"use client";
-import React from "react";
-import axios from "axios";
+"use client"
+import React from 'react';
+import useSWR from 'swr';
 
-function ArticlesComponent({ articles }) {
+function ArticlesComponent() {
+  const baseUrl = process.env.NEXT_PUBLIC_URL_API;
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSWR(`${baseUrl}/api/articles?populate=*`, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  if (!Array.isArray(data.data)) return <div>Data is not in the expected format</div>;
+
   return (
-    <>
-      {articles.map((article, index) => (
-        <div key={index}>
-          <div className="card w-96 min-h-400 glass shadow-xl">
+    <div>
+      {data.data.map((item, index) => {
+        const { title, subtitle, description, author, createdAt, updatedAt, publishedAt, picture } = item.attributes;
+
+        // Mengambil URL gambar dalam format large jika tersedia
+        const largeImageUrl = picture.data?.[0]?.attributes?.formats?.large?.url
+          ? `${baseUrl}${picture.data[0].attributes.formats.large.url}`
+          : null;
+
+        return (
+          <div key={index} className="card w-96 min-h-400 glass shadow-xl">
             <figure className="w-96 h-64">
-              {article.largeImageUrl && (
-                <img
-                  className="w-full h-full object-cover"
-                  src={article.largeImageUrl}
-                  alt={article.title}
-                />
+              {largeImageUrl && (
+                <img className="w-full h-full object-cover" src={largeImageUrl} alt={title}></img>
               )}
             </figure>
             <div className="card-body">
               <h2 className="card-title">
-                {article.title}
-                <div className="badge badge-secondary">{article.author}</div>
+                {title}
+                <div className="badge badge-secondary">{author}</div>
               </h2>
-              <p>{article.subtitle}</p>
+              <p>{subtitle}</p>
               <div className="collapse">
                 <input type="checkbox" />
                 <div className="collapse-title text-xl font-medium">
-                  description
+                  Description
                 </div>
                 <div className="collapse-content">
-                  <p>{article.description}</p>
+                  <p>{description}</p>
                 </div>
               </div>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
+                <div className="badge badge-outline">Published: {new Date(publishedAt).toLocaleDateString()}</div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </>
+        );
+      })}
+    </div>
   );
-}
-
-export async function getStaticProps() {
-  const baseUrl = process.env.NEXT_PUBLIC_URL_API;
-  let articles = [];
-  try {
-    // Menggunakan axios untuk melakukan request
-    const response = await axios.get(`${baseUrl}/api/articles?populate=*`);
-
-    // Mengolah data yang diterima
-    articles = response.data.data.map((item) => {
-      //Destructuring Assignment
-      const {
-        title,
-        subtitle,
-        description,
-        author,
-        createdAt,
-        updatedAt,
-        publishedAt,
-        picture,
-      } = item.attributes;
-      //Optional Chaining (?.) || Ternary Operator (? :)
-      const largeImageUrl = picture.data?.[0]?.attributes?.formats?.large?.url
-        ? //String Interpolation (Template Literals)
-          `${baseUrl}${picture.data[0].attributes.formats.large.url}`
-        : null;
-
-      return {
-        title,
-        subtitle,
-        description,
-        author,
-        createdAt,
-        updatedAt,
-        publishedAt,
-        largeImageUrl,
-      };
-    });
-  } catch (error) {
-    console.error("There was a problem fetching the articles:", error);
-  }
-  return {
-    props: {
-      articles,
-    },
-    revalidate: 60, // Regenerasi halaman setiap 60 detik
-  };
 }
 
 export default ArticlesComponent;
--- app/article/page.js
+++ app/article/page.js
@@ -1,5 +1,16 @@
-import ArticlesComponent from "./article"
+"use client"
+import React from 'react';
+import useSWR from 'swr';
 export default function page() {
+  const baseUrl = process.env.NEXT_PUBLIC_URL_API;
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSWR(`${baseUrl}/api/articles?populate=*`, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  if (!Array.isArray(data.data)) return <div>Data is not in the expected format</div>;
+
     return (
       <>
         <header className="bg-gray-800 text-white p-4">
@@ -7,7 +18,43 @@ export default function page() {
     </header>
     <main className="p-4">
         <div className="grid grid-cols-3 gap-4">
-        <ArticlesComponent/>
+        {data.data.map((item, index) => {
+        const { title, subtitle, description, author, createdAt, updatedAt, publishedAt, picture } = item.attributes;
+
+        // Mengambil URL gambar dalam format large jika tersedia
+        const largeImageUrl = picture.data?.[0]?.attributes?.formats?.large?.url
+          ? `${baseUrl}${picture.data[0].attributes.formats.large.url}`
+          : null;
+
+        return (
+          <div key={index} className="card w-96 min-h-400 glass shadow-xl">
+            <figure className="w-96 h-64">
+              {largeImageUrl && (
+                <img className="w-full h-full object-cover" src={largeImageUrl} alt={title}></img>
+              )}
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {title}
+                <div className="badge badge-secondary">{author}</div>
+              </h2>
+              <p>{subtitle}</p>
+              <div className="collapse">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                  Description
+                </div>
+                <div className="collapse-content">
+                  <p>{description}</p>
+                </div>
+              </div>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Published: {new Date(publishedAt).toLocaleDateString()}</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
         </div>
     </main>
     <footer className="bg-gray-700 text-white p-4 text-center">
